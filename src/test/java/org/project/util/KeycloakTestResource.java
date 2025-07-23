4a133c25cec1b0a34fe852aae7234349605d3537
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-07-23 19:01:26 +0400

    Feat test resource for keycloack

---
+++ src/test/java/org/project/util/KeycloakTestResource.java
@@ -0,0 +1,33 @@
+package org.project.util;
+
+import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+import org.testcontainers.containers.GenericContainer;
+
+import java.util.Map;
+
+public class KeycloakTestResource implements QuarkusTestResourceLifecycleManager {
+
+    GenericContainer<?> keycloakContainer;
+
+    @Override
+    public Map<String, String> start() {
+        keycloakContainer = new GenericContainer<>("quay.io/keycloak/keycloak:26.2")
+                .withExposedPorts(7080)
+                .withEnv("REDHAT_FIPS", "false");
+
+        keycloakContainer.start();
+
+        String host = keycloakContainer.getHost();
+        int port = keycloakContainer.getMappedPort(7080);
+        String keycloakURL = "http://%s:%s/auth/realms/karto-realm".formatted(host, port);
+        return Map.of("keycloak-url", keycloakURL);
+    }
+
+    @Override
+    public void stop() {
+        if (keycloakContainer != null) {
+            keycloakContainer.stop();
+            keycloakContainer = null;
+        }
+    }
+}
