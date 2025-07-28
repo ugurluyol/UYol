d69f109adbd7a9331f0d7d37ffe199151d09abbd
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-07-28 21:03:59 +0400

    Feat better global exception hadnling

--- src/main/java/org/project/infrastructure/exceptions_handler/ExceptionHandler.java
+++ src/main/java/org/project/infrastructure/exceptions_handler/ExceptionHandler.java
@@ -1,24 +1,60 @@
 package org.project.infrastructure.exceptions_handler;
 
 import io.quarkus.logging.Log;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
+
+import org.project.application.dto.common.ErrorMessage;
 import org.project.domain.shared.exceptions.DomainException;
+import org.project.domain.user.exceptions.BannedUserException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hadzhy.jetquerious.exceptions.NotFoundException;
 
 @Provider
 public class ExceptionHandler implements ExceptionMapper<Throwable> {
 
+  static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
   @Override
   public Response toResponse(Throwable e) {
     Log.error("Global error handler: ", e);
-    if (e instanceof DomainException) return Response
-            .status(Response.Status.BAD_REQUEST)
-            .entity(e.getMessage())
-            .build();
+    if (e instanceof BannedUserException)
+      return Response
+          .status(Response.Status.FORBIDDEN)
+          .entity(errorMessage(e.getMessage()))
+          .type(MediaType.APPLICATION_JSON)
+          .build();
+
+    if (e instanceof DomainException)
+      return Response
+          .status(Response.Status.BAD_REQUEST)
+          .entity(errorMessage(e.getMessage()))
+          .type(MediaType.APPLICATION_JSON)
+          .build();
+
+    if (e instanceof NotFoundException)
+      return Response
+          .status(Response.Status.NOT_FOUND)
+          .type(MediaType.APPLICATION_JSON)
+          .build();
 
     return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-            .entity("Unexpected error occurred. Please contact support.")
-            .build();
+        .entity(errorMessage("Unexpected error occurred. Please contact support."))
+        .type(MediaType.APPLICATION_JSON)
+        .build();
+  }
+
+  private static String errorMessage(String message) {
+    try {
+      String result = OBJECT_MAPPER.writeValueAsString(new ErrorMessage(message));
+      return result;
+    } catch (JsonProcessingException e) {
+      Log.error("Unexpected error. Cannot process json in exception mapper. " + e.getMessage());
+      return null;
+    }
   }
 }
