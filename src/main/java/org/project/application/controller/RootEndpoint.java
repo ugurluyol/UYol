953f83701bca23e9627f3c00f042fb600445230c
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-08-01 00:40:47 +0400

    Feat root endpoint

---
+++ src/main/java/org/project/application/controller/RootEndpoint.java
@@ -0,0 +1,8 @@
+package org.project.application.controller;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/uyol")
+public class RootEndpoint extends Application {
+}
