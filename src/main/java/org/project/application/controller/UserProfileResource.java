5a9416ba83098635b37b36ff45657d3150e0a050
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-08-01 02:24:00 +0400

    Feat endpoints for profile picture

---
+++ src/main/java/org/project/application/controller/UserProfileResource.java
@@ -0,0 +1,46 @@
+package org.project.application.controller;
+
+import java.io.InputStream;
+
+import org.eclipse.microprofile.jwt.JsonWebToken;
+import org.project.application.dto.profile.ProfilePictureDTO;
+import org.project.application.service.UserProfileService;
+import org.project.domain.user.value_objects.ProfilePicture;
+
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/user/profile")
+@RolesAllowed("USER")
+public class UserProfileResource {
+
+  private final JsonWebToken jwt;
+
+  private final UserProfileService profile;
+
+  UserProfileResource(UserProfileService profile, JsonWebToken jwt) {
+    this.profile = profile;
+    this.jwt = jwt;
+  }
+
+  @GET
+  @Path("/picture")
+  public Response picture() {
+    ProfilePicture profilePicture = profile.profilePictureOf(jwt.getName());
+    return Response.ok(
+        new ProfilePictureDTO(profilePicture.profilePicture(), profilePicture.imageType())).build();
+  }
+
+  @PUT
+  @Path("/picture/change")
+  @Consumes(MediaType.APPLICATION_OCTET_STREAM)
+  public Response changePicture(InputStream inputStream) {
+    profile.changeProfilePictureOf(jwt.getName(), inputStream);
+    return Response.accepted().build();
+  }
+}
---
+++ src/main/java/org/project/application/service/UserProfileService.java
@@ -0,0 +1,46 @@
+package org.project.application.service;
+
+import java.io.InputStream;
+
+import org.project.domain.user.entities.User;
+import org.project.domain.user.factories.IdentifierFactory;
+import org.project.domain.user.repositories.UserRepository;
+import org.project.domain.user.value_objects.ProfilePicture;
+import org.project.infrastructure.files.ProfilePictureRepository;
+import org.project.infrastructure.files.StreamUtils;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.core.Response.Status;
+
+import static org.project.application.util.RestUtil.required;
+import static org.project.application.util.RestUtil.responseException;
+
+@ApplicationScoped
+public class UserProfileService {
+
+  private final UserRepository userRepository;
+
+  private final ProfilePictureRepository pictureRepository;
+
+  UserProfileService(UserRepository userRepository, ProfilePictureRepository pictureRepository) {
+    this.userRepository = userRepository;
+    this.pictureRepository = pictureRepository;
+  }
+
+  public void changeProfilePictureOf(String identifier, InputStream inputStream) {
+    required("Picture", inputStream);
+    User user = userRepository.findBy(IdentifierFactory.from(identifier)).orElseThrow();
+
+    byte[] pictureBytes = StreamUtils.toByteArray(inputStream)
+        .orElseThrow(() -> responseException(Status.BAD_REQUEST, "Invalid picture provided."));
+   
+    user.profilePicture(ProfilePicture.of(pictureBytes, user));
+    pictureRepository.put(user);
+  }
+
+  public ProfilePicture profilePictureOf(String identifier) {
+    User user = userRepository.findBy(IdentifierFactory.from(identifier)).orElseThrow();
+    return pictureRepository.load(ProfilePicture.profilePicturePath(user))
+        .orElseThrow(() -> responseException(Status.NOT_FOUND, "Profile picture not found."));
+  }
+}
