1102b7e9c7d711f754de9f4a884c1d4cbfb374a0
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-08-01 01:46:02 +0400

    Feat marker interface for user identifier

--- src/main/java/org/project/domain/user/value_objects/Email.java
+++ src/main/java/org/project/domain/user/value_objects/Email.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 
 import org.project.domain.shared.exceptions.IllegalDomainArgumentException;
 
-public record Email(String email) {
+public record Email(String email) implements Identifier {
 
     private static final String emailRegex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
 
@@ -19,13 +19,17 @@ public record Email(String email) {
     }
 
     public static void validate(String email) {
-        if (Objects.isNull(email)) throw new IllegalDomainArgumentException("Email can`t be null");
-        if (email.isBlank()) throw new IllegalDomainArgumentException("Email can`t be blank");
-        if (email.length() > MAX_SIZE) throw new IllegalDomainArgumentException("Email is too long");
+        if (Objects.isNull(email))
+            throw new IllegalDomainArgumentException("Email can`t be null");
+        if (email.isBlank())
+            throw new IllegalDomainArgumentException("Email can`t be blank");
+        if (email.length() > MAX_SIZE)
+            throw new IllegalDomainArgumentException("Email is too long");
 
         String[] splitEmail = email.split("@");
 
-        if (splitEmail.length != 2) throw new IllegalDomainArgumentException("Invalid email format.");
+        if (splitEmail.length != 2)
+            throw new IllegalDomainArgumentException("Invalid email format.");
         if (splitEmail[0].isEmpty() || splitEmail[0].length() > 64)
             throw new IllegalDomainArgumentException("Invalid email format.");
 
@@ -42,18 +46,18 @@ public record Email(String email) {
         return email;
     }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (!(o instanceof Email))
-			return false;
-		Email other = (Email) o;
-		return Objects.equals(email, other.email);
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Email))
+            return false;
+        Email other = (Email) o;
+        return Objects.equals(email, other.email);
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(email);
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(email);
+    }
 }
--- src/main/java/org/project/domain/user/value_objects/Phone.java
+++ src/main/java/org/project/domain/user/value_objects/Phone.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 
 import org.project.domain.shared.exceptions.IllegalDomainArgumentException;
 
-public record Phone(String phoneNumber) {
+public record Phone(String phoneNumber) implements Identifier {
 
     public static final String PHONE_NUMBER_REGEX = "^(\\+\\d{1,3}( )?)?((\\(\\d{1,3}\\))|\\d{1,3})[- .]?\\d{3,4}[- .]?\\d{4}$";
     public static final Pattern PHONE_NUMBER_PATTERN = Pattern.compile(PHONE_NUMBER_REGEX);
@@ -17,12 +17,16 @@ public record Phone(String phoneNumber) {
     }
 
     public static void validate(String phoneNumber) {
-        if (phoneNumber == null) throw new IllegalDomainArgumentException("Phone number can`t be null");
-        if (phoneNumber.isBlank()) throw new IllegalDomainArgumentException("Phone number should`t be blank.");
-        if (phoneNumber.length() > MAX_SIZE) throw new IllegalDomainArgumentException("Phone number is too long");
+        if (phoneNumber == null)
+            throw new IllegalDomainArgumentException("Phone number can`t be null");
+        if (phoneNumber.isBlank())
+            throw new IllegalDomainArgumentException("Phone number should`t be blank.");
+        if (phoneNumber.length() > MAX_SIZE)
+            throw new IllegalDomainArgumentException("Phone number is too long");
 
         Matcher matcher = PHONE_NUMBER_PATTERN.matcher(phoneNumber);
-        if (!matcher.matches()) throw new IllegalDomainArgumentException("Invalid phone number.");
+        if (!matcher.matches())
+            throw new IllegalDomainArgumentException("Invalid phone number.");
     }
 
     @Override
@@ -30,19 +34,19 @@ public record Phone(String phoneNumber) {
         return phoneNumber;
     }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (!(o instanceof Phone))
-			return false;
-		Phone phone = (Phone) o;
-		return Objects.equals(phoneNumber, phone.phoneNumber);
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Phone))
+            return false;
+        Phone phone = (Phone) o;
+        return Objects.equals(phoneNumber, phone.phoneNumber);
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(phoneNumber);
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(phoneNumber);
+    }
 
 }
