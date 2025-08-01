c2e4de7349279928ae7b03f9ccedade8de3aa025
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-08-02 00:09:39 +0400

    Feat contract for driver repository

--- src/main/java/org/project/domain/driver/repositories/DriverRepository.java
+++ src/main/java/org/project/domain/driver/repositories/DriverRepository.java
@@ -1,4 +1,17 @@
 package org.project.domain.driver.repositories;
 
+import org.project.domain.driver.entities.Driver;
+import org.project.domain.driver.value_objects.DriverID;
+import org.project.domain.driver.value_objects.UserID;
+import org.project.domain.shared.containers.Result;
+
 public interface DriverRepository {
+
+    Result<Integer, Throwable> save(Driver driver);
+
+    Result<Integer, Throwable> updateLicense(Driver driver);
+
+    Result<Driver, Throwable> findBy(DriverID driverID);
+
+    Result<Driver, Throwable> findBy(UserID userID);
 }
