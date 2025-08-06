de1e3f99013b878363b0b5c715a3bf904c9a00b1
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-08-06 23:47:18 +0400

    Feat value object for location

---
+++ src/main/java/org/project/domain/ride/value_object/Location.java
@@ -0,0 +1,22 @@
+package org.project.domain.ride.value_object;
+
+import org.project.domain.shared.exceptions.IllegalDomainArgumentException;
+
+public record Location(String description, double latitude, double longitude) {
+
+  public Location {
+    if (description == null || description.isBlank())
+      throw new IllegalDomainArgumentException("Description is required");
+
+    if (latitude < -90.0 || latitude > 90.0)
+      throw new IllegalDomainArgumentException("Latitude must be between -90 and 90");
+
+    if (longitude < -180.0 || longitude > 180.0)
+      throw new IllegalDomainArgumentException("Longitude must be between -180 and 180");
+  }
+
+  @Override
+  public String toString() {
+    return description + " (" + latitude + ", " + longitude + ")";
+  }
+}
