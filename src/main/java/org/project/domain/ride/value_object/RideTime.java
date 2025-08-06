8bdbc97b49942c05a818bdce86bd41d4619dce89
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-08-07 01:20:34 +0400

    additional value objects

--- src/main/java/org/project/domain/ride/value_object/RideTime.java
+++ src/main/java/org/project/domain/ride/value_object/RideTime.java
@@ -3,16 +3,18 @@ package org.project.domain.ride.value_object;
 import static org.project.domain.shared.util.Utils.required;
 
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 
 import org.project.domain.shared.exceptions.IllegalDomainArgumentException;
 
-public record RideTime(LocalDateTime date, LocalTime time) {
+public record RideTime(LocalDateTime startOfTheTrip, LocalDateTime endOfTheTrip) {
   public RideTime {
-    required("date", date);
-    required("time", time);
+    required("startOfTheTrip", startOfTheTrip);
+    required("endOfTheTrip", endOfTheTrip);
 
-    if (!date.isAfter(LocalDateTime.now()) && !time.isAfter(LocalTime.now()))
-      throw new IllegalDomainArgumentException("Ride must be scheduled for the future");
+    if (!startOfTheTrip.isAfter(LocalDateTime.now()))
+      throw new IllegalDomainArgumentException("Start of the trip must be in the future");
+
+    if (!endOfTheTrip.isAfter(startOfTheTrip))
+      throw new IllegalDomainArgumentException("End of the trip must be after start time");
   }
 }
