372f5d39566ca17afaa61a930ee9f2233e772dc4
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-08-10 19:55:37 +0400

    Feat tests for ride creation and status change

--- src/test/java/org/project/domain/RideTest.java
+++ src/test/java/org/project/domain/RideTest.java
@@ -1,4 +1,90 @@
 package org.project.domain;
 
-public class RideTest {
+import org.junit.jupiter.api.Test;
+import org.project.domain.ride.entities.Ride;
+import org.project.domain.ride.enumerations.RideStatus;
+import org.project.domain.shared.exceptions.IllegalDomainArgumentException;
+import org.project.features.TestDataGenerator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RideTest {
+    
+    @Test
+    void validCreationOfRideWithoutDelivery() {
+        assertDoesNotThrow(() -> assertNotNull(TestDataGenerator.rideWithoutDelivery()));
+    }
+
+    @Test
+    void validCreationOfRideWithDelivery() {
+        assertDoesNotThrow(() -> assertNotNull(TestDataGenerator.rideWithDelivery()));
+    }
+
+    @Test
+    void shouldStartTheRoadSuccessfully() {
+        Ride ride = TestDataGenerator.rideWithoutDelivery();
+        assertDoesNotThrow(ride::start);
+        assertEquals(RideStatus.ON_THE_ROAD, ride.status());
+    }
+
+    @Test
+    void invalidRideStartOnCanceledRide() {
+        Ride ride = TestDataGenerator.rideWithoutDelivery();
+        ride.cancel();
+
+        assertThrows(IllegalDomainArgumentException.class, ride::start, "Ride already started or canceled/finished");
+    }
+
+    @Test
+    void invalidRideStartOnFinishedRide() {
+        Ride ride = TestDataGenerator.rideWithoutDelivery();
+        ride.start();
+        ride.finish();
+        assertThrows(IllegalDomainArgumentException.class, ride::start, "Ride already started or canceled/finished");
+    }
+
+    @Test
+    void shouldCancelRideSuccessfully() {
+        Ride ride = TestDataGenerator.rideWithoutDelivery();
+        assertDoesNotThrow(ride::cancel);
+        assertEquals(RideStatus.CANCELED, ride.status());
+    }
+
+    @Test
+    void shouldNotAllowToCancelRideThatAlreadyOnTheRoad() {
+        Ride ride = TestDataGenerator.rideWithoutDelivery();
+        ride.start();
+        assertThrows(IllegalDomainArgumentException.class, ride::cancel,
+                "Ride cancellation is not possible if it`s already on the road or finished");
+    }
+
+    @Test
+    void shouldNotAllowToCancelRideThatFinished() {
+        Ride ride = TestDataGenerator.rideWithoutDelivery();
+        ride.start();
+        ride.finish();
+        assertThrows(IllegalDomainArgumentException.class, ride::cancel,
+                "Ride cancellation is not possible if it`s already on the road or finished");
+    }
+
+    @Test
+    void shouldFinishRideSuccessfully() {
+        Ride ride = TestDataGenerator.rideWithoutDelivery();
+        ride.start();
+        assertDoesNotThrow(ride::finish);
+        assertEquals(RideStatus.ENDED_SUCCESSFULLY, ride.status());
+    }
+
+    @Test
+    void shouldNotFinishRideThatNotStarter() {
+        Ride ride = TestDataGenerator.rideWithoutDelivery();
+        assertThrows(IllegalDomainArgumentException.class, ride::finish, "You can`t finish the ride which was not going");
+    }
+
+    @Test
+    void shouldNotFinishRideThatAlreadyCanceled() {
+        Ride ride = TestDataGenerator.rideWithoutDelivery();
+        ride.cancel();
+        assertThrows(IllegalDomainArgumentException.class, ride::finish, "You can`t finish the ride which was not going");
+    }
 }
