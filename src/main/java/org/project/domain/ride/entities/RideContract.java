b44839f4a1640fdad8bcbbb99767e1812c31a068
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-08-12 18:44:54 +0400

    Feat ride contract entity

--- src/main/java/org/project/domain/ride/entities/RideContract.java
+++ src/main/java/org/project/domain/ride/entities/RideContract.java
@@ -1,71 +1,60 @@
 package org.project.domain.ride.entities;
 
-import org.project.domain.ride.enumerations.SeatStatus;
+import org.project.domain.ride.value_object.BookedSeats;
 import org.project.domain.ride.value_object.Price;
 import org.project.domain.ride.value_object.RideID;
-import org.project.domain.shared.exceptions.IllegalDomainArgumentException;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 import static org.project.domain.shared.util.Utils.required;
 
 public class RideContract {
-
-    public record PassengerSeat(int index, SeatStatus status) {}
-
-    private final RideID rideId;
+    private final RideID rideID;
     private final Price pricePerSeat;
-    private final List<PassengerSeat> bookedSeats;
+    private final BookedSeats bookedSeats;
 
-    public RideContract(RideID rideId, Price pricePerSeat) {
-        required("rideId", rideId);
+    private RideContract(RideID rideID, Price pricePerSeat, BookedSeats bookedSeats) {
+        required("rideId", rideID);
         required("pricePerSeat", pricePerSeat);
+        required("bookedSeats", bookedSeats);
 
-        this.rideId = rideId;
+        this.rideID = rideID;
         this.pricePerSeat = pricePerSeat;
-        this.bookedSeats = new ArrayList<>();
+        this.bookedSeats = bookedSeats;
     }
 
-    public RideID rideId() {
-        return rideId;
+    public static RideContract of(RideID rideId, Price pricePerSeat, BookedSeats bookedSeats) {
+        return new RideContract(rideId, pricePerSeat, bookedSeats);
+    }
+
+    public static RideContract fromRepository(RideID rideId,  Price pricePerSeat, BookedSeats bookedSeats) {
+        return new RideContract(rideId, pricePerSeat, bookedSeats);
+    }
+
+    public RideID rideID() {
+        return rideID;
     }
 
     public Price pricePerSeat() {
         return pricePerSeat;
     }
 
-    public List<PassengerSeat> bookedSeats() {
-        return new ArrayList<>(bookedSeats);
-    }
-
-    public void addPassenger(int seatIndex, SeatStatus status) {
-        required("status", status);
-
-        boolean alreadyBooked = bookedSeats.stream()
-                .anyMatch(s -> s.index() == seatIndex);
-        if (alreadyBooked)
-            throw new IllegalDomainArgumentException("This seat is already booked in the contract.");
-
-        bookedSeats.add(new PassengerSeat(seatIndex, status));
+    public BookedSeats bookedSeats() {
+        return bookedSeats;
     }
 
     public boolean hasSeat(int seatIndex) {
-        return bookedSeats.stream().anyMatch(s -> s.index() == seatIndex);
+        return bookedSeats.bookedSeats().stream().anyMatch(s -> s.index() == seatIndex);
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof RideContract that)) return false;
-        return Objects.equals(rideId, that.rideId) &&
-               Objects.equals(pricePerSeat, that.pricePerSeat) &&
-               Objects.equals(bookedSeats, that.bookedSeats);
+        if (o == null || getClass() != o.getClass()) return false;
+        RideContract that = (RideContract) o;
+        return Objects.equals(rideID, that.rideID);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(rideId, pricePerSeat, bookedSeats);
+        return Objects.hashCode(rideID);
     }
 }
