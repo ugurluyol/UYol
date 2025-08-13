eb96b0ab77c57cad1037633058ad422a6d5dfe5e
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-08-13 19:43:32 +0400

    Feat endpoint for driver registration

--- src/main/java/org/project/application/controller/DriverResource.java
+++ src/main/java/org/project/application/controller/DriverResource.java
@@ -1,4 +1,31 @@
 package org.project.application.controller;
 
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.enterprise.inject.Instance;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.jwt.JsonWebToken;
+import org.project.application.service.DriverService;
+
+@Path("/driver")
+@RolesAllowed("USER")
 public class DriverResource {
+
+    private final JsonWebToken jwt;
+
+    private final DriverService service;
+
+    DriverResource(Instance<JsonWebToken> jwt, DriverService service) {
+        this.jwt = jwt.get();
+        this.service = service;
+    }
+
+    @POST
+    @Path("/registration")
+    public Response registerDriver(@QueryParam("driver_license") String driverLicense) {
+        service.register(jwt.getName(), driverLicense);
+        return Response.accepted().build();
+    }
 }
--- src/main/java/org/project/application/service/DriverService.java
+++ src/main/java/org/project/application/service/DriverService.java
@@ -1,4 +1,42 @@
 package org.project.application.service;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.core.Response;
+import org.project.domain.fleet.entities.Driver;
+import org.project.domain.fleet.repositories.DriverRepository;
+import org.project.domain.fleet.value_objects.DriverLicense;
+import org.project.domain.fleet.value_objects.UserID;
+import org.project.domain.user.entities.User;
+import org.project.domain.user.factories.IdentifierFactory;
+import org.project.domain.user.repositories.UserRepository;
+
+import static org.project.application.util.RestUtil.responseException;
+
+@ApplicationScoped
 public class DriverService {
+
+    private final UserRepository userRepository;
+
+    private final DriverRepository driverRepository;
+
+    DriverService(UserRepository userRepository, DriverRepository driverRepository) {
+        this.userRepository = userRepository;
+        this.driverRepository = driverRepository;
+    }
+
+    public void register(String identifier, String driverLicense) {
+        DriverLicense license = new DriverLicense(driverLicense);
+        User user = userRepository.findBy(IdentifierFactory.from(identifier)).orElseThrow();
+
+        if (driverRepository.isDriverExists(new UserID(user.id())))
+            throw responseException(Response.Status.CONFLICT, "Driver is already registered on this user account.");
+
+        if (driverRepository.isLicenseExists(license))
+            throw responseException(Response.Status.CONFLICT, "This driver license is already registered.");
+
+        Driver driver = Driver.of(new UserID(user.id()), license);
+        driverRepository.save(driver)
+                .orElseThrow(()  -> responseException(Response.Status.INTERNAL_SERVER_ERROR,
+                        "Unable to process your request at the moment. Please try again."));
+    }
 }
