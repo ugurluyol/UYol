67ebc2ecd2f2595399fa3a74d839d71788be7dd2
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-08-13 19:48:51 +0400

    Feat endpoint for owner registration

--- src/main/java/org/project/application/controller/OwnerResource.java
+++ src/main/java/org/project/application/controller/OwnerResource.java
@@ -1,4 +1,31 @@
 package org.project.application.controller;
 
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.enterprise.inject.Instance;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.jwt.JsonWebToken;
+import org.project.application.service.OwnerService;
+
+@Path("/owner")
+@RolesAllowed("USER")
 public class OwnerResource {
+
+    private final JsonWebToken jwt;
+
+    private final OwnerService service;
+
+    OwnerResource(Instance<JsonWebToken> jwt, OwnerService service) {
+        this.jwt = jwt.get();
+        this.service = service;
+    }
+
+    @POST
+    @Path("/register")
+    public Response registerOwner(@QueryParam("voen") String voen) {
+        service.register(jwt.getName(), voen);
+        return Response.accepted().build();
+    }
 }
--- src/main/java/org/project/application/service/OwnerService.java
+++ src/main/java/org/project/application/service/OwnerService.java
@@ -1,4 +1,43 @@
 package org.project.application.service;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.core.Response;
+import org.project.domain.fleet.entities.Owner;
+import org.project.domain.fleet.repositories.OwnerRepository;
+import org.project.domain.fleet.value_objects.UserID;
+import org.project.domain.fleet.value_objects.Voen;
+import org.project.domain.user.entities.User;
+import org.project.domain.user.factories.IdentifierFactory;
+import org.project.domain.user.repositories.UserRepository;
+
+import static org.project.application.util.RestUtil.responseException;
+
+@ApplicationScoped
 public class OwnerService {
+
+    private final UserRepository userRepository;
+
+    private final OwnerRepository ownerRepository;
+
+    OwnerService(UserRepository userRepository, OwnerRepository ownerRepository) {
+        this.userRepository = userRepository;
+        this.ownerRepository = ownerRepository;
+    }
+
+    public void register(String identifier, String voenRaw) {
+        Voen voen = new Voen(voenRaw);
+        User user = userRepository.findBy(IdentifierFactory.from(identifier)).orElseThrow();
+        UserID userID = new UserID(user.id());
+
+        if (ownerRepository.isOwnerExists(userID))
+            throw responseException(Response.Status.CONFLICT, "Owner is already registered to this account");
+
+        if (ownerRepository.isVoenExists(voen))
+            throw responseException(Response.Status.CONFLICT, "Voen is already used");
+
+        Owner owner = Owner.of(userID, voen);
+        ownerRepository.save(owner)
+                .orElseThrow(() -> responseException(Response.Status.CONFLICT,
+                        "Unable to process your request at the moment. Please try again."));
+    }
 }
