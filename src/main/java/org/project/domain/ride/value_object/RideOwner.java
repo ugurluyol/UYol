7ede1d261f118aaf39ba74f1a40116aa22eb8b3b
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-08-13 19:44:56 +0400

    Feat required changes in Ride model

--- src/main/java/org/project/domain/ride/value_object/RideOwner.java
+++ src/main/java/org/project/domain/ride/value_object/RideOwner.java
@@ -1,13 +1,13 @@
 package org.project.domain.ride.value_object;
 
+import org.project.domain.shared.annotations.Nullable;
 import org.project.domain.shared.value_objects.DriverID;
 import org.project.domain.shared.value_objects.OwnerID;
 
 import static org.project.domain.shared.util.Utils.required;
 
-public record RideOwner(DriverID driverID, OwnerID ownerID) {
+public record RideOwner(DriverID driverID, @Nullable OwnerID ownerID) {
     public RideOwner {
         required("driverID", driverID);
-        required("ownerID", ownerID);
     }
 }
