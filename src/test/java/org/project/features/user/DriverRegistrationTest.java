054ce055b9c250fb0beeb625cf241ab8ef715181
gunel aslanova <aslanovagunel409@gmail.com>
2025-08-15 16:55:31 +0400

    Add integration tests for Driver registration endpoint using JWT tokens

--- src/test/java/org/project/features/user/DriverRegistrationTest.java
+++ src/test/java/org/project/features/user/DriverRegistrationTest.java
@@ -1,52 +1,110 @@
 package org.project.features.user;
 
+import static io.restassured.RestAssured.given;
+
+import org.junit.jupiter.api.Test;
+import org.project.domain.fleet.value_objects.DriverLicense;
+import org.project.domain.user.entities.User;
+import org.project.domain.user.repositories.UserRepository;
+import org.project.features.PostgresTestResource;
+import org.project.features.TestDataGenerator;
+import org.project.infrastructure.security.JWTUtility;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
+
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
-import org.junit.jupiter.api.Test;
-import org.project.application.dto.auth.RegistrationForm;
-import org.project.features.PostgresTestResource;
-import org.project.features.TestDataGenerator;
-import org.project.features.util.DBManagementUtils;
 
 @QuarkusTest
 @QuarkusTestResource(PostgresTestResource.class)
 class DriverRegistrationTest {
 
     @Inject
-    DBManagementUtils dbManagement;
+	UserRepository userRepository;
 
-    @Test
+	@Inject
+	JWTUtility jwtUtility;
+
+	@Test
     void successfullyRegisterDriver() throws JsonProcessingException {
-        RegistrationForm form = TestDataGenerator.generateRegistrationForm();
-        dbManagement.saveAndVerifyUser(form);
+		User user = TestDataGenerator.user();
+		userRepository.save(user);
+		String jwtToken = jwtUtility.generateToken(user);
+
+		DriverLicense license = TestDataGenerator.driverLicense();
+
+		given().header("Authorization", "Bearer " + jwtToken).queryParam("driver_license", license.licenseNumber())
+				.when().post("uyol/driver/registration").then().statusCode(202);
+	}
+
+	@Test
+	void invalidDriverLicense() throws JsonProcessingException {
+		User user = TestDataGenerator.user();
+		userRepository.save(user);
+		String jwtToken = jwtUtility.generateToken(user);
+
+		given().header("Authorization", "Bearer " + jwtToken).queryParam("driver_license", "").when()
+				.post("uyol/driver/registration").then().statusCode(400);
+	}
+
+	@Test
+	void invalidIdentifier() {
+		User fakeUser = TestDataGenerator.user();
+		String jwtToken = jwtUtility.generateToken(fakeUser);
+
+		DriverLicense license = TestDataGenerator.driverLicense();
+
+		given().header("Authorization", "Bearer " + jwtToken).queryParam("driver_license", license.licenseNumber())
+				.when().post("uyol/driver/registration").then().statusCode(404);
+    }
+
+	@Test
+	void userAccountDontExists() {
+		User fakeUser = TestDataGenerator.user();
+		String jwtToken = jwtUtility.generateToken(fakeUser);
+
+		DriverLicense license = TestDataGenerator.driverLicense();
+
+		given().header("Authorization", "Bearer " + jwtToken).queryParam("driver_license", license.licenseNumber())
+				.when().post("uyol/driver/registration").then().statusCode(404);
+	}
+
+    @Test
+	void driverAlreadyExists() throws JsonProcessingException {
+		User user = TestDataGenerator.user();
+		userRepository.save(user);
+		String jwtToken = jwtUtility.generateToken(user);
+
+		DriverLicense license = TestDataGenerator.driverLicense();
 
 
+		given().header("Authorization", "Bearer " + jwtToken).queryParam("driver_license", license.licenseNumber())
+				.when().post("uyol/driver/registration").then().statusCode(202);
+
+
+		given().header("Authorization", "Bearer " + jwtToken).queryParam("driver_license", license.licenseNumber())
+				.when().post("uyol/driver/registration").then().statusCode(409);
     }
 
     @Test
-    void invalidDriverLicense() {
+	void driverLicenseAlreadyExists() throws JsonProcessingException {
 
-    }
+		User user1 = TestDataGenerator.user();
+		userRepository.save(user1);
+		String jwtToken1 = jwtUtility.generateToken(user1);
 
-    @Test
-    void invalidIdentifier() {
+		DriverLicense license = TestDataGenerator.driverLicense();
 
-    }
+		given().header("Authorization", "Bearer " + jwtToken1).queryParam("driver_license", license.licenseNumber())
+				.when().post("uyol/driver/registration").then().statusCode(202);
 
-    @Test
-    void userAccountDontExists() {
 
-    }
+		User user2 = TestDataGenerator.user();
+		userRepository.save(user2);
+		String jwtToken2 = jwtUtility.generateToken(user2);
 
-    @Test
-    void driverAlreadyExists() {
-
-    }
-
-    @Test
-    void driverLicenseAlreadyExists() {
-
+		given().header("Authorization", "Bearer " + jwtToken2).queryParam("driver_license", license.licenseNumber())
+				.when().post("uyol/driver/registration").then().statusCode(409);
     }
 }
