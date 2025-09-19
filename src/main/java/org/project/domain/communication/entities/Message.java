215ab7bdb30e4c5a4c333bb34ba07048e86ed080
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-09-19 15:35:00 +0400

    feat: dates for message

--- src/main/java/org/project/domain/communication/entities/Message.java
+++ src/main/java/org/project/domain/communication/entities/Message.java
@@ -3,9 +3,8 @@ package org.project.domain.communication.entities;
 import org.project.domain.communication.value_objects.ConversationID;
 import org.project.domain.communication.value_objects.MessageContent;
 import org.project.domain.communication.value_objects.MessageID;
+import org.project.domain.shared.value_objects.Dates;
 import org.project.domain.shared.value_objects.UserID;
-
-import java.time.LocalDateTime;
 
 import static org.project.domain.shared.util.Utils.required;
 
@@ -14,7 +13,7 @@ public record Message(
         ConversationID conversationID,
         UserID sender,
         MessageContent content,
-        LocalDateTime createdAt,
+        Dates dates,
         boolean edited) {
 
     public Message {
@@ -22,14 +21,14 @@ public record Message(
         required("conversationID", conversationID);
         required("sender", sender);
         required("content", content);
-        required("createdAt", createdAt);
+        required("dates", dates);
     }
 
     static Message create(ConversationID convId, UserID sender, MessageContent content) {
-        return new Message(MessageID.newID(), convId, sender, content, LocalDateTime.now(), false);
+        return new Message(MessageID.newID(), convId, sender, content, Dates.defaultDates(), false);
     }
 
     Message edit(MessageContent newContent) {
-        return new Message(messageID, conversationID, sender, newContent, createdAt, true);
+        return new Message(messageID, conversationID, sender, newContent, dates.updated(), true);
     }
 }
