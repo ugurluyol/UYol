136b3bccffe8cd8e8264324e6ca20e32b7e92930
HaciHaciyev <gadzievgadzi508@gmail.com>
2025-11-10 17:44:28 +0400

    feat: api methods for conversation model

--- src/main/java/org/project/domain/communication/entities/Conversation.java
+++ src/main/java/org/project/domain/communication/entities/Conversation.java
@@ -1,4 +1,17 @@
 package org.project.domain.communication.entities;
 
+import org.project.domain.communication.enumerations.ConversationStatus;
+import org.project.domain.communication.value_objects.ConversationID;
+import org.project.domain.communication.value_objects.Participant;
+import org.project.domain.ride.value_object.RideID;
+import org.project.domain.shared.value_objects.Dates;
+import org.project.domain.shared.value_objects.DriverID;
+
 public sealed interface Conversation permits ActiveConversation, ClosedConversation, BlockedConversation {
+    ConversationID conversationID();
+    RideID rideID();
+    Participant participant();
+    DriverID driverID();
+    Dates dates();
+    ConversationStatus status();
 }
